f9e5d7dc1b9d456aec75f15ce5b58cf98a9c4cbb
DownCloud <100362519+SelectND@users.noreply.github.com>
2022-05-06 18:57:10 +0200

    Add files via upload
    Added main file

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+if __name__ == '__main__':
+    # defining the base construction
+    length = int(input("Password length: "))
+    chars = list(string.ascii_letters + string.digits + r"!@#$%^&*()")
+    random.shuffle(chars)
+
+    # creating the password as an array
+    passwd = []
+    for i in range(length):
+        passwd.append(random.choice(chars))
+    random.shuffle(passwd)
+
+    # converting array to string and printing the string
+    finalPassword = ("".join(passwd))
+    print(finalPassword)
